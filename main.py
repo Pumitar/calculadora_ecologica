487bf46777b75b6882ba2b7e9c4dd62479ff857a
Pumitar <joao.puma.1411@gmail.com>
2025-01-18 12:34:18 -0500

    Agregando mas opciones

--- main.py
+++ main.py
@@ -51,11 +51,13 @@ def form():
 def submit_form():
     # Declarar variables para la recogida de datos
     name = request.form['name']
-
+    email = request.form['email']
+    address = request.form['address']
+    date = request.form['date']
     # Puedes guardar tus datos o enviarlos por correo electrónico
     return render_template('form_result.html', 
                            # Coloque aquí las variables
-                           name=name,
+                           name=name, email=email, address=address,date=date
                            )
 
 app.run(debug=True)
